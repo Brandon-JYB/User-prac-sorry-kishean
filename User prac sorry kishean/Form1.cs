47d038587a017b7da51ea6fb7f5d477826cb2bf0
Brandon <Brandon@LAPTOP-PJJKP1L8>
2026-04-17 22:21:16 +0800

    Add project files.

---
+++ User prac sorry kishean/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace User_prac_sorry_kishean
+{
+    public partial class Form1 : Form
+    {
+        string connStr = "server=localhost;user=root;password=;database=;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                                                    //Column Name   //('{}')
+                    string query = $"INSERT INTO user (name) VALUES ('{textBox1.Text}')";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+    }
